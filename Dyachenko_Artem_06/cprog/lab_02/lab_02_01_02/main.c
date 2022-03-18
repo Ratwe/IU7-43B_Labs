aede427838521f91fe9db8c60a12ec112082e44d
Dyachenko Artem IU7-13B <artemon1324@yandex.ru>
2022-03-18 12:15:55 +0000

    ADD: cprog lab 2 task 1

--- Dyachenko_Artem_06/cprog/lab_02/lab_02_01_02/main.c
+++ Dyachenko_Artem_06/cprog/lab_02/lab_02_01_02/main.c
@@ -5,8 +5,66 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define N 10
+#define ZERO_DIVISION 1
+
+// Ввод массива
+void get_array(int arr[], size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+}
+
+// Подсчёт среднего отрицательных чисел
+double calc_mid_of_negative(int arr[], size_t n)
+{
+    double sum = 0;
+    int count = 0;
+
+    for (size_t i = 0; i < n; i++)
+        if (arr[i] < 0)
+        {
+            sum += arr[i];
+            count++;
+        }
+
+    if (!count)
+        return ZERO_DIVISION;
+    return sum / count;
+}
 
 int main(void)
 {
-	return 0;
+    int n;
+    printf("Введите кол-во элементов массива: ");
+    scanf("%d", &n);
+
+    #ifdef DEBUG
+    printf("[DB]: n = %d\n", n);
+    #endif
+
+    int arr[N] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    get_array(arr, n);
+
+    // Вывод массива
+    #ifdef DEBUG
+    printf("[DB]: arr = ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    #endif
+
+    if (calc_mid_of_negative(arr, n) > 0)
+    {
+        printf("Ошибка: в массиве нет отрицательных чисел!\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("Среднее арифметическое отрицательных чисел массива: %f\n", \
+           calc_mid_of_negative(arr, n));
+
+    return EXIT_SUCCESS;
 }
