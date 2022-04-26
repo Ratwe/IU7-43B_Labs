7d1edeb2bc9916c56a61778f28c29636709f5bd5
Kuznetsov Egor IU7-23B <egor-kuznetsov2004@mail.ru>
2022-04-26 21:29:27 +0300

    FIX: delete old folder

--- Kuznetov_Egor_12/c/lab_1/lab_01_01/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in *.out *.o *.exe func_tests/scripts/*.txt *.gc*; do
-    rm "$i" 2> /dev/null
-done
-exit 0
--- Kuznetov_Egor_12/c/lab_2/lab_02_04_00/build_release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -O3 -std=c99 -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -Wvla -c main.c -lm
-gcc main.o -o app.exe
